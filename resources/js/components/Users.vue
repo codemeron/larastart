178281b16a6bdcfb3c495076086b0d77f2a1b94b
codemeron <codemeron00@gmail.com>
2020-07-19 17:49:51 +0800

    Added users.vue

---
+++ resources/js/components/Users.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <div class="row mt-5">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-header">
+                <h3 class="card-title">Users Table</h3>
+
+                <div class="card-tools">
+                    <button class="btn btn-success">Add New <i class="fas fa-user-plus fa-fw"></i></button>
+                </div>
+              </div>
+              <!-- /.card-header -->
+              <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Email</th>
+                      <th>Type</th>
+                      <th>Controls</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>183</td>
+                      <td>John Doe</td>
+                      <td>11-7-2014</td>
+                      <td><span class="tag tag-success">Approved</span></td>
+                      <td>
+                          <a href="#" title="Edit">
+                              <i class="fas fa-edit text-blue"></i>
+                          </a> 
+                          <a href="#" title="Delete">
+                              <i class="fas fa-trash text-red"></i>
+                          </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
